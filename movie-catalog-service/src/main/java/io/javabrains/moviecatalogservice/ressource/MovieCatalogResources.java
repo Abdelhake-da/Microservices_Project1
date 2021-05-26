63f162b832d8dceb00b16998a11615f1ad250ac6
Abdelhake-da <abdelhake.da@gmail.com>
2021-05-26 21:18:42 +0100

    this is second way for printing all movies

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/ressource/MovieCatalogResources.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/ressource/MovieCatalogResources.java
@@ -3,6 +3,7 @@ package io.javabrains.moviecatalogservice.ressource;
 import io.javabrains.moviecatalogservice.models.CatalogItem;
 import io.javabrains.moviecatalogservice.models.Movie;
 import io.javabrains.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,18 +18,20 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResources {
+    @Autowired
+    //yhawas 3al @Bean li 3andah nafs type ta3 variable li tahtah
+    private RestTemplate restTemplate;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId")String userId){
         //get all rated movie Ids
-        RestTemplate restTemplat=new RestTemplate();
 
         List<Rating> ratings = Arrays.asList(
                 new Rating("1234",3),
                 new Rating("321",4)
         );
         return ratings.stream().map(rating-> {
-           Movie movie= restTemplat.getForObject("http://localhost:8082/movies/"+rating.getMovieId(),Movie.class);
+           Movie movie= restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(),Movie.class);
             return new CatalogItem(movie.getName(), "test", rating.getRating());
         })
                 .collect(Collectors.toList());
