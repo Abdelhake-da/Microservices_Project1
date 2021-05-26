021a52d1360a920c283aa7dd4fa26d8a07376dbc
Abdelhake-da <abdelhake.da@gmail.com>
2021-05-27 00:16:42 +0100

    this is the third one

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -3,7 +3,20 @@ package io.javabrains.moviecatalogservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.ExchangeFunction;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriBuilderFactory;
+
+import java.nio.file.Watchable;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
@@ -12,6 +25,10 @@ public class MovieCatalogServiceApplication {
 public  RestTemplate getRestTemplate(){
 	return new RestTemplate();
 }
+public WebClient.Builder getWebClientBuilder() {
+	return  WebClient.builder();
+}
+
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogServiceApplication.class, args);
 	}
--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/ressource/MovieCatalogResources.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/ressource/MovieCatalogResources.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 
 import java.util.Arrays;
@@ -21,9 +22,13 @@ public class MovieCatalogResources {
     @Autowired
     //yhawas 3al @Bean li 3andah nafs type ta3 variable li tahtah
     private RestTemplate restTemplate;
-
+    @Autowired
+    private  WebClient.Builder webclientBuilder;
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId")String userId){
+        //
+        WebClient.Builder builder = WebClient.builder();
+
         //get all rated movie Ids
 
         List<Rating> ratings = Arrays.asList(
@@ -31,7 +36,13 @@ public class MovieCatalogResources {
                 new Rating("321",4)
         );
         return ratings.stream().map(rating-> {
-           Movie movie= restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(),Movie.class);
+          // Movie movie= restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(),Movie.class);
+
+            Movie movie=webclientBuilder
+                    .build().get()
+                    .uri("http://localhost:8082/movies/"+rating.getMovieId())
+                    .retrieve().bodyToMono(Movie.class).block();
+
             return new CatalogItem(movie.getName(), "test", rating.getRating());
         })
                 .collect(Collectors.toList());
